6cf360af4566455c32d44d6c94a16fcd82d9fb8c
H-H-Army-projects <130982814+H-H-Army-projects@users.noreply.github.com>
2023-04-17 14:03:45 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,155 @@
+from os import system
+from colorama import Fore as f
+from pyfiglet import figlet_format 
+from translate import Translator
+
+
+system("cls")
+
+print(f"""{f.BLUE}
+
+
+ ▄▄▄          ██▀███      ██▓    ▄▄▄          ███▄ ▄███▓   ▓█████  ██░ ██  ██▀███  
+▒████▄       ▓██ ▒ ██▒   ▓██▒   ▒████▄       ▓██▒▀█▀ ██▒   ▓█   ▀ ▓██░ ██▒▓██ ▒ ██▒
+▒██  ▀█▄     ▓██ ░▄█ ▒   ▒██▒   ▒██  ▀█▄     ▓██    ▓██░   ▒███   ▒██▀▀██░▓██ ░▄█ ▒
+░██▄▄▄▄██    ▒██▀▀█▄     ░██░   ░██▄▄▄▄██    ▒██    ▒██    ▒▓█  ▄ ░▓█ ░██ ▒██▀▀█▄  
+ ▓█   ▓██▒   ░██▓ ▒██▒   ░██░    ▓█   ▓██▒   ▒██▒   ░██▒   ░▒████▒░▓█▒░██▓░██▓ ▒██▒
+ ▒▒   ▓▒█░   ░ ▒▓ ░▒▓░   ░▓      ▒▒   ▓▒█░   ░ ▒░   ░  ░   ░░ ▒░ ░ ▒ ░░▒░▒░ ▒▓ ░▒▓░
+  ▒   ▒▒ ░     ░▒ ░ ▒░    ▒ ░     ▒   ▒▒ ░   ░  ░      ░    ░ ░  ░ ▒ ░▒░ ░  ░▒ ░ ▒░
+  ░   ▒        ░░   ░     ▒ ░     ░   ▒      ░      ░         ░    ░  ░░ ░  ░░   ░ 
+      ░  ░      ░         ░           ░  ░          ░         ░  ░ ░  ░  ░   ░     
+                                                                                   
+                        
+                         {f.RED}[
+	                      All Rights Protected{f.WHITE}
+	                     {f.CYAN}    Coder : {f.WHITE}Host1let
+	                     {f.BLUE}      ID : {f.GREEN}@GOD__NUM__1{f.WHITE}
+	                           {f.RED}                      ]
+
+                               
+
+""")
+
+
+print(f"""{f.BLUE}[{f.RED}1{f.BLUE}] Sites For Info of Hitler
+{f.BLUE}[{f.RED}2{f.BLUE}] Create a Banner
+{f.BLUE}[{f.RED}3{f.BLUE}] Open IDs in Rubika
+{f.BLUE}[{f.RED}4{f.BLUE}] Name of Guns and Tanks in WW2
+{f.BLUE}[{f.RED}5{f.BLUE}] Translate Persian To German
+{f.BLUE}[{f.RED}6{f.BLUE}] Translate German To Persian
+{f.BLUE}[{f.RED}7{f.BLUE}] Sites For Info of Nazism
+
+""")
+
+chose = str(input(f"{f.LIGHTBLACK_EX}set{f.RED} >{f.YELLOW} "))
+
+if chose == "1":
+    system("cls")
+    print(f"""{f.RED}-------------------------------------
+{f.RED}Here I Have Some Sites For You :)   |
+-------------------------------------\n
+{f.YELLOW}[{f.RED}1{f.YELLOW}]{f.CYAN} https://en.wikipedia.org/wiki/Adolf_Hitler
+{f.YELLOW}[{f.RED}2{f.YELLOW}]{f.CYAN} https://www.britannica.com/biography/Adolf-Hitler
+{f.YELLOW}[{f.RED}3{f.YELLOW}]{f.CYAN} https://www.historyhit.com/facts-about-adolf-hitler
+{f.YELLOW}[{f.RED}4{f.YELLOW}]{f.CYAN} https://history.as.uky.edu/hitler-essential-background-information
+{f.YELLOW}[{f.RED}5{f.YELLOW}]{f.CYAN} https://www.history.com/topics/world-war-ii/adolf-hitler-1
+{f.YELLOW}[{f.RED}6{f.YELLOW}]{f.CYAN} https://www.nationalww2museum.org/war/articles/how-did-adolf-hitler-happen
+{f.YELLOW}[{f.RED}7{f.YELLOW}]{f.CYAN} https://encyclopedia.ushmm.org/content/en/article/adolf-hitler-early-years-1889-1913
+{f.YELLOW}[{f.RED}8{f.YELLOW}]{f.CYAN} https://www.history.com/topics/world-war-ii/world-war-ii-history
+{f.YELLOW}[{f.RED}9{f.YELLOW}]{f.CYAN} https://www.bbc.co.uk/teach/adolf-hitler-man-and-monster/zbrx8xs
+
+{f.RED}----------------
+{f.RED}YouTube Videos |
+----------------
+
+{f.YELLOW}[{f.RED}1{f.YELLOW}]{f.MAGENTA} https://www.youtube.com/watch?v=llEXqB4YyiE
+{f.YELLOW}[{f.RED}2{f.YELLOW}]{f.MAGENTA} https://www.youtube.com/watch?v=6XnsYZxH2nI
+{f.YELLOW}[{f.RED}3{f.YELLOW}]{f.MAGENTA} https://www.youtube.com/watch?v=khd2EOiAdjE""")
+    
+
+elif chose == "2":
+    name = str(input(f"{f.LIGHTBLACK_EX}write a name{f.RED} >{f.YELLOW} "))
+    system("cls")
+    font_list = ['1943____','3-d','3x5','4x4_offr','5lineoblique','5x7','5x8','64f1____','6x10','6x9','a_zooloo','acrobatic','advenger','alligator','alligator2','alphabet','aquaplan','arrows','asc_____','ascii___','assalt_m','asslt__m','avatar','b_m__200','banner','banner3','banner3-D','banner4','barbwire','basic','battle_s','battlesh','baz__bil','beer_pub','bell','big','bigchief','binary','block','brite','briteb','britebi','britei','broadway','bubble','bubble__','bubble_b','bulbhead','c1______','c2______','c_ascii_','c_consen','calgphy2','caligraphy','catwalk','caus_in_','char1___','char2___','char3___','char4___','charact1','charact2','charact3','charact4','charact5','charact6','characte','charset_','chartr','chartri','chunky','clb6x10','clb8x10','clb8x8','cli8x8','clr5x10','clr5x6','clr5x8','clr6x10','clr6x6','clr6x8','clr7x10','clr7x8','clr8x10','clr8x8','xbriteb','xbritebi','xbritei','xchartr','xchartri','xcour','xcourb','xcourbi','xcouri','xhelv','xhelvb','xhelvbi','xhelvi','xsans','xsansb','xsansbi','xsansi','xsbook','xsbookb','xsbookbi','xsbooki','xtimes','xtty','xttyb','yie-ar__','yie_ar_k','z-pilot_','zig_zag_','war_of_w','slant','isometric1','isometric2','isometric3','isometric4']
+    for f in font_list:
+        b = figlet_format(text=name, font=f)+"\n"+"\n"
+        print(b)
+
+elif chose == "3":
+    id_list = ["black_hitler", "black_nazism", "heil_hitler", "zed_bts_for_ever", "mental_torturer", "bts_is_animal", "black_hat_nazism", "dark_face_hitler", "hitler_dark_face", "make_cum_bts", "bts_fucker_1"]
+    print(f"{f.RED}ok ... ")
+    for ids in id_list:
+        print(f"\n{f.YELLOW}{ids}{f.WHITE}\n")
+
+
+elif chose == "4":
+    print(f"""
+    
+{f.RED}+{f.BLUE}--- {f.WHITE}name{f.BLUE} ---{f.RED}+    +{f.BLUE}--- {f.WHITE}type{f.BLUE} -----{f.RED}+    
+
+{f.YELLOW}M1 Garand           {f.CYAN}Battle rifle
+
+{f.YELLOW}M1903 Springfield   {f.CYAN}Battle rifle
+
+{f.YELLOW}M1941 Johnson rifle  {f.CYAN}Battle rifle
+
+{f.YELLOW}M1917 Enfield rifle   {f.CYAN}Standard rifle
+
+{f.YELLOW}Winchester Model 1895   {f.CYAN}Standard rifle
+
+{f.YELLOW}M1903 Springfield       {f.CYAN}Startdard rifle
+
+{f.YELLOW}M1903A4 sniper rifle   {f.CYAN}Sniper rifle
+
+{f.YELLOW}Winchester M12         {f.CYAN}Shotgun
+
+{f.YELLOW}Browning Auto-5        {f.CYAN}Shotgun
+
+{f.YELLOW}Remington 31          {f.CYAN}Shotgun
+
+{f.YELLOW}Stevens M520-30      {f.CYAN}Shotgun
+
+{f.YELLOW}M2 Light tank       {f.CYAN}Tank
+
+{f.YELLOW}M3 Stuart           {f.CYAN}Tank
+
+{f.YELLOW}M5 Stuart           {f.CYAN}Tank
+
+{f.YELLOW}M26 Super Pershing   {f.CYAN}Heavy Tank{f.WHITE}
+
+""")
+
+elif chose == "5":
+    user = input(f"{f.BLUE}Enter text for translating{f.RED}>{f.YELLOW} ")
+    options = Translator(from_lang='persian', to_lang='german')
+    translate = options.translate(user)
+    print(translate)
+
+elif chose == "6":
+    user = input(f"{f.BLUE}Enter text for translating{f.RED}>{f.YELLOW} ")
+    options = Translator(from_lang='german', to_lang='persian')
+    translate = options.translate(user)
+    print(translate)
+
+
+elif chose == "7":
+    print(f"""
+{f.RED}----------------
+{f.RED}|{f.YELLOW}Sites For You {f.RED}|
+{f.RED}----------------
+
+{f.YELLOW}[{f.RED}1{f.YELLOW}]{f.GREEN} https://en.wikipedia.org/wiki/Nazism
+{f.YELLOW}[{f.RED}2{f.YELLOW}]{f.GREEN} https://en.wikipedia.org/wiki/Nazi_Germany
+{f.YELLOW}[{f.RED}3{f.YELLOW}]{f.GREEN} https://www.britannica.com/topic/Nazi-Party
+{f.YELLOW}[{f.RED}4{f.YELLOW}]{f.GREEN} https://www.nationalww2museum.org/war/articles/the-nuremberg-trial-and-its-legacy 
+{f.YELLOW}[{f.RED}5{f.YELLOW}]{f.GREEN} https://www.yadvashem.org/holocaust/about/nazi-germany-1933-39/beginning-of-persecution.html
+{f.YELLOW}[{f.RED}6{f.YELLOW}]{f.GREEN} https://www.smithsonianmag.com/history/true-story-reichstag-fire-and-nazis-rise-power-180962240/
+{f.YELLOW}[{f.RED}7{f.YELLOW}]{f.GREEN} https://www.cairn-int.info/article-E_VIN_117_0043--how-the-nazis-viewed-history.htm
+
+{f.RED}--------------
+{f.RED}|{f.MAGENTA}Youtube Link{f.RED}|
+--------------
+
+{f.YELLOW}[{f.RED}1{f.YELLOW}]{f.BLUE}https://www.youtube.com/watch?v=yEk6zGYwyhc
+
+""")
